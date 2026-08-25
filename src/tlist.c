d20bcc0cfa7a96af1b9d4b9af2e7522a95817c03
Simon Long <simon@raspberrypi.com>
2026-08-25 09:12:16 +0100

    Move menu cache to core panel code

--- src/tlist.c
+++ src/tlist.c
@@ -29,9 +29,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <glib/gi18n.h>
 #include <gio/gdesktopappinfo.h>
 #include <gdk/gdkwayland.h>
-#include <menu-cache.h>
 
-#include "lxutils.h"
+#include "plugin.h"
 #include "launcher.h"
 
 #include "tlist.h"
@@ -85,11 +84,8 @@ static void create_or_update_button (WinlistPlugin *wl, WindowItem *item);
 static void set_icon (WinlistPlugin *wl, WindowBtn *item);
 static void destroy_button (gpointer data);
 static gboolean update_button_states (WinlistPlugin *wl);
-static float score_match (const char *str1, const char *str2);
-static char *get_exe (const char *cmdline);
-static char *menu_cache_id (WinlistPlugin *wl, const char *app_id);
 static void popup_menu (GtkWidget *widget, gpointer userdata);
-static void launch_id (WinlistPlugin *wl, GtkWidget *widget);
+static void launch_id (GtkWidget *widget);
 static char *get_string (char *cmd);
 static char *find_alternative (const char *launch_id);
 static void add_launcher (WinlistPlugin *wl, char *id);
@@ -506,7 +502,7 @@ static void unminimise_app (GtkWidget *wid, gpointer userdata)
 static WindowBtn *find_btn (WinlistPlugin *wl, WindowItem *item)
 {
     GList *btns = wl->buttons;
-    char *mcid = menu_cache_id (wl, item->app_id);
+    char *mcid = menu_cache_id (item->app_id);
     while (btns)
     {
         WindowBtn *btn = (WindowBtn *) btns->data;
@@ -572,7 +568,7 @@ static void create_or_update_button (WinlistPlugin *wl, WindowItem *item)
 
 static void set_icon (WinlistPlugin *wl, WindowBtn *item)
 {
-    char *str, *id, *buf;
+    char *str, *buf;
     GAppInfo *info;
     GIcon *ic;
     GtkStyleContext *sc;
@@ -602,12 +598,7 @@ static void set_icon (WinlistPlugin *wl, WindowBtn *item)
     else
     {
         // the desktop file name isn't valid, so search the menu cache for something similar
-        id = menu_cache_id (wl, item->app_id);
-        str = g_strdup_printf ("%s.desktop", id);
-        g_free (id);
-        mitem = menu_cache_find_item_by_id (wl->menu_cache, str);
-        g_free (str);
-
+        mitem = get_cache_item (item->app_id);
         if (mitem)
         {
             str = g_strdup (menu_cache_item_get_icon (mitem));
@@ -711,197 +702,6 @@ static gboolean update_button_states (WinlistPlugin *wl)
         btns = g_list_next (btns);
     }
     return FALSE;
-}
-
-/*----------------------------------------------------------------------------*/
-/* Menu cache search                                                          */
-/*----------------------------------------------------------------------------*/
-
-/* This is an attempt to score how similar two strings are by comparing how many letters
- * at the start of each are identical, and how many letters at the end are identical.
- * It's not perfect... */
-
-static float score_match (const char *str1, const char *str2)
-{
-    int score, pos1, pos2;
-    char *str1l, *str2l;
-    float result;
-
-    if (!str1 || !str2) return 0.0;
-
-    str1l = g_ascii_strdown (str1, -1);
-    str2l = g_ascii_strdown (str2, -1);
-    score = 0;
-
-    // count matching characters from start
-    pos1 = 0;
-    while (str1l[pos1] && str2l[pos1] && str1l[pos1] == str2l[pos1])
-    {
-        score++;
-        pos1++;
-    }
-
-    // count matching characters from end
-    pos1 = strlen (str1l) - 1;
-    pos2 = strlen (str2l) - 1;
-    while (pos1 && pos2 && str1l[pos1] == str2l[pos2])
-    {
-        score++;
-        pos1--;
-        pos2--;
-    }
-
-    result = score;
-    if (strlen (str1l) > strlen (str2l)) result /= strlen (str2l);
-    else result /= strlen (str1l);
-
-    g_free (str1l);
-    g_free (str2l);
-
-    return result;
-}
-
-static char *get_exe (const char *cmdline)
-{
-    // g_path_get_basename fails with quoted paths, so...
-    char *buf, *start, *end, *ret;
-    char del;
-
-    buf = g_strdup (cmdline);
-
-    start = buf;
-    if (strchr ("'\"", *start))
-    {
-        del = *start;
-        start++;
-    }
-    else del = ' ';
-
-    end = start;
-    while (*end)
-    {
-        if (*end == del) break;
-        end++;
-    }
-    *end = 0;
-
-    if (strrchr (start, '/')) start = strrchr (start, '/') + 1;
-    ret = g_strdup (start);
-    g_free (buf);
-
-    return ret;
-}
-
-static char *menu_cache_id (WinlistPlugin *wl, const char *app_id)
-{
-    MenuCacheItem *item;
-    GSList *list, *iter;
-    GAppInfo *info;
-    char *id, *exec, *s2, *best = NULL;
-    float res, score;
-    const char *ex, *s1;
-
-    // loop through the cache to find the best match
-    score = 0.0;
-    list = menu_cache_list_all_apps (wl->menu_cache);
-    iter = list;
-    while (iter)
-    {
-        item = (MenuCacheItem *) iter->data;
-
-        // first check that the cache item is a valid desktop info, i.e. has an associated exec
-        id = g_strdup (menu_cache_item_get_id (item));
-        info = (GAppInfo *) g_desktop_app_info_new (id);
-        if (!info)
-        {
-            g_free (id);
-            iter = iter->next;
-            continue;
-        }
-        else g_object_unref (info);
-
-        // strip the .desktop from the end for matching purposes
-        *strrchr (id, '.') = 0;
-
-        // if there is a caseless match with the app-id, this is correct - return it
-        if (!g_ascii_strncasecmp (app_id, id, 1000))
-        {
-            g_slist_free_full (list, (GDestroyNotify) ((void *) menu_cache_item_unref));
-            return id;
-        }
-
-        // try matching the part of the id after a final .
-        s1 = strrchr (app_id, '.') ? strrchr (app_id, '.') + 1 : app_id;
-        s2 = strrchr (id, '.') ? strrchr (id, '.') + 1 : id;
-        if (!g_ascii_strncasecmp (s1, s2, 1000))
-        {
-            g_slist_free_full (list, (GDestroyNotify) ((void *) menu_cache_item_unref));
-            return id;
-        }
-
-        iter = iter->next;
-    }
-
-    // no joy - try matching executable names
-    iter = list;
-    while (iter)
-    {
-        item = (MenuCacheItem *) iter->data;
-
-        // first check that the cache item is a valid desktop info, i.e. has an associated exec
-        id = g_strdup (menu_cache_item_get_id (item));
-        info = (GAppInfo *) g_desktop_app_info_new (id);
-        if (!info)
-        {
-            g_free (id);
-            iter = iter->next;
-            continue;
-        }
-        else g_object_unref (info);
-
-        // strip the .desktop from the end for matching purposes
-        *strrchr (id, '.') = 0;
-
-        // get the executable name
-        ex = menu_cache_app_get_exec ((MenuCacheApp *) item);
-        if (ex) exec = get_exe (ex);
-        else exec = NULL;
-
-        // if there is a caseless match with the executable, this is correct - return it
-        if (exec && !g_ascii_strncasecmp (app_id, exec, 1000))
-        {
-            g_free (exec);
-            if (best) g_free (best);
-            g_slist_free_full (list, (GDestroyNotify) ((void *) menu_cache_item_unref));
-            return id;
-        }
-
-        // look for matching characters at start and end
-        res = score_match (app_id, id);
-        if (res > score)
-        {
-            score = res;
-            if (best) g_free (best);
-            best = g_strdup (id);
-        }
-
-        if (exec)
-        {
-            res = score_match (app_id, exec);
-            if (res > score)
-            {
-                score = res;
-                if (best) g_free (best);
-                best = g_strdup (id);
-            }
-            g_free (exec);
-        }
-
-        g_free (id);
-        iter = iter->next;
-    }
-    g_slist_free_full (list, (GDestroyNotify) ((void *) menu_cache_item_unref));
-    return best;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -1034,12 +834,12 @@ static void popup_menu (GtkWidget *widget, gpointer userdata)
 /* Launchers                                                                  */
 /*----------------------------------------------------------------------------*/
 
-static void launch_id (WinlistPlugin *wl, GtkWidget *widget)
+static void launch_id (GtkWidget *widget)
 {
     char *lid, *str;
     GAppInfo *info;
 
-    lid = menu_cache_id (wl, gtk_widget_get_name (widget));
+    lid = menu_cache_id (gtk_widget_get_name (widget));
     str = g_strdup_printf ("%s.desktop", lid);
     info = (GAppInfo *) g_desktop_app_info_new (str);
 
@@ -1181,7 +981,7 @@ static gboolean handle_button_release (GtkWidget *wid, GdkEventButton *event, gp
 
     switch (event->button)
     {
-        case 1:     if (!btn->windows || !activate_app (wid, userdata)) launch_id (wl, wid);
+        case 1:     if (!btn->windows || !activate_app (wid, userdata)) launch_id (wid);
                     return FALSE;
 
         case 3:     popup_menu (wid, btn);
@@ -1345,10 +1145,6 @@ void wlist_init (WinlistPlugin *wl)
     wl->windows = NULL;
     wl->buttons = NULL;
 
-    gboolean need_prefix = (g_getenv ("XDG_MENU_PREFIX") == NULL);
-    wl->menu_cache = menu_cache_lookup (need_prefix ? "lxde-applications.menu+hidden" : "applications.menu+hidden");
-    wl->reload_notify = menu_cache_add_reload_notify (wl->menu_cache, NULL, NULL);
-
     GdkDisplay *gdk_display = gdk_display_get_default ();
     struct wl_display *display = gdk_wayland_display_get_wl_display (gdk_display);
     wl->registry = wl_display_get_registry (display);
@@ -1371,12 +1167,6 @@ void wlist_destructor (gpointer user_data)
 
     if (wl->buttons) g_list_free_full (wl->buttons, (GDestroyNotify) destroy_button);
     wl->buttons = NULL;
-
-    if (wl->menu_cache)
-    {
-        menu_cache_remove_reload_notify (wl->menu_cache, wl->reload_notify);
-        // unref'ing the menu cache causes a segfault because its io thread isn't being closed...
-    }
 
     if (wl->registry) wl_registry_destroy (wl->registry);
     wl->registry = NULL;
